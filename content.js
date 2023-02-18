0ebb3919a526b2c411d9e09580d32204f75bac33
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-02-18 09:41:20 +0100

    Add files via upload

---
+++ content.js
@@ -0,0 +1,14 @@
+document.head.insertAdjacentHTML("beforeend",
+  `<style>
+    .dggLuL {background: white; object-fit: contain;}
+    .SearchResultsstyles__SearchResultsWrapper-sc-kmhlm3-1 img:hover {
+      transform: scale(3.0) !important; 
+      bottom: 15vh !important; 
+      position: relative; cursor: pointer; 
+      background: #00000047 !important;
+      float:left !important;
+    } 
+      .fpkjmq{z-index:99;}
+      
+      </style>`);
+
